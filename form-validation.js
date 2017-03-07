04489cc752614f2dcab97cbff6462747d84fa200
Cory Wall <cwall4@mail.csuchico.edu>
2017-03-07 11:15:40 -0800

    check for good password. blank password field or username field now generate a specific error

--- form-validation.js
+++ form-validation.js
@@ -4,10 +4,17 @@ function validate(){
   var passEntered = document.getElementById("pass").value;
 
   if (userEntered.length >= 6) {
-    document.getElementById("usernameError").innerHTML="";
+    document.getElementById("usernameError").innerHTML="Good username";
     document.getElementById("usernameGroup").classList.remove("has-error");
     document.getElementById("usernameGroup").classList.add("has-success");
-  } else {
+  } else if (userEntered.length == 0) {
+    //Show message that there is an error with the username...
+    document.getElementById("usernameError").innerHTML="Username field cannot be left blank";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }else {
     //Show message that there is an error with the username...
     document.getElementById("usernameError").innerHTML="Bad username.";
     document.getElementById("usernameError").classList.remove("hidden-message");
@@ -16,11 +23,23 @@ function validate(){
     document.getElementById("usernameGroup").classList.add("has-error");
   }
 
-
-  //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+  if (passEntered.length == 0) {
+    //Show message that there is an error with the password...
+    document.getElementById("passwordError").innerHTML="Password field cannot be left blank";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  } else if (passEntered != "password") {
+      document.getElementById("passwordError").innerHTML="Good password.";
+      document.getElementById("passwordGroup").classList.remove("has-error");
+      document.getElementById("passwordGroup").classList.add("has-success");
+  } else {
+    //Show message that there is an error with the password...
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
 }
