a3075317a23ccb4a80f164455c2824864c5c5d19
Cory Wall <cwall4@mail.csuchico.edu>
2017-03-07 11:21:22 -0800

    added comments fixed spacing

--- form-validation.js
+++ form-validation.js
@@ -4,8 +4,11 @@ function validate(){
   var passEntered = document.getElementById("pass").value;
 
   if (userEntered.length >= 6) {
+    //Displays a message confirming that a valid username was entered
     document.getElementById("usernameError").innerHTML="Good username";
+    //Removes the error class (turns items red)
     document.getElementById("usernameGroup").classList.remove("has-error");
+    //Adds the success class (turns items green)
     document.getElementById("usernameGroup").classList.add("has-success");
   } else if (userEntered.length == 0) {
     //Show message that there is an error with the username...
@@ -14,7 +17,7 @@ function validate(){
     document.getElementById("usernameError").classList.add("shown-message");
     //Turn the username items red
     document.getElementById("usernameGroup").classList.add("has-error");
-  }else {
+  } else {
     //Show message that there is an error with the username...
     document.getElementById("usernameError").innerHTML="Bad username.";
     document.getElementById("usernameError").classList.remove("hidden-message");
@@ -23,6 +26,7 @@ function validate(){
     document.getElementById("usernameGroup").classList.add("has-error");
   }
 
+  //Testing for blank password field
   if (passEntered.length == 0) {
     //Show message that there is an error with the password...
     document.getElementById("passwordError").innerHTML="Password field cannot be left blank";
@@ -31,9 +35,12 @@ function validate(){
     //Turn the password items red
     document.getElementById("passwordGroup").classList.add("has-error");
   } else if (passEntered != "password") {
-      document.getElementById("passwordError").innerHTML="Good password.";
-      document.getElementById("passwordGroup").classList.remove("has-error");
-      document.getElementById("passwordGroup").classList.add("has-success");
+    //Displays a message confirming a valid password was entered
+    document.getElementById("passwordError").innerHTML="Good password.";
+    //Removes the error class (turns items red)
+    document.getElementById("passwordGroup").classList.remove("has-error");
+    //Adds the success class (turns items green)
+    document.getElementById("passwordGroup").classList.add("has-success");
   } else {
     //Show message that there is an error with the password...
     document.getElementById("passwordError").innerHTML="Bad password.";
